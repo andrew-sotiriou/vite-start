91e26efd6194989bf12fa50c6f05fc178e4f24ad
Andrew Sotiriou <drew.sotiriou@gmail.com>
2026-02-01 11:45:01 -0500

    Update to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Set the base path for your GitHub Pages site
-  base: '/',
+  base: '/vite-start/',
 
   // Include framework plugins here if necessary
   // plugins: [], 
