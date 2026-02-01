9b0618a6473a4fb3d22f05e7bdc42b7b473a2a20
Andrew Sotiriou <drew.sotiriou@gmail.com>
2026-02-01 11:42:32 -0500

    Added a vite config file and working on setting up githib pages to work with vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Set the base path for your GitHub Pages site
+  base: '/',
+
+  // Include framework plugins here if necessary
+  // plugins: [], 
+});
